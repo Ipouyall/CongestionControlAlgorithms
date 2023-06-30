4f69914cdeaad4778d5055a173a37185ecfe083a
Ali_Hodaei <ali.n.hodaei@gmail.com>
2023-06-30 15:34:31 +0330

    Reno & NewReno updated.

--- src/Reno.cpp
+++ src/Reno.cpp
@@ -1,18 +1,21 @@
-#include "NewReno.h"
+#include "Reno.h"
 
 using namespace std;
 
 
-NewReno::NewReno() :Reno() {
-
+Reno::Reno()
+    :TCPConnection() 
+    {
 }
 
-NewReno::NewReno(int cwnd_, int rtt_, int time_out_, bool fast_retransmit_, bool fast_recovery_)
-            :Reno(cwnd_, rtt_, time_out_, fast_retransmit_, fast_recovery_)  {
+Reno::Reno(int cwnd_, int rtt_, int time_out_, bool fast_retransmit_, bool fast_recovery_)
+            :TCPConnection(cwnd_, rtt_, time_out_, fast_retransmit_, fast_recovery_) 
+             {
 }
 
 
-vector<Packet> NewReno::SendData(pair<int, bool> lost_packet_verified) {
+
+vector<Packet> Reno::SendData(pair<int, bool> lost_packet_verified) {
     vector<Packet> data;
     int count = 0;
     if (lost_packet_verified.first != -1)
@@ -20,11 +23,7 @@ vector<Packet> NewReno::SendData(pair<int, bool> lost_packet_verified) {
     for (int i = window.last_ack + 1; i <= window.last_written; i++) {
         if (count == cwnd)
             return data;
-        if (sent_data[i].is_sack && sent_data[i].is_ack == false) {
-            sent_data[i].is_sent = true;
-            data.push_back(sent_data[i]);
-        }
-        else if (sent_data[i].is_ack == false && !sent_data[i].is_sack) {
+        if (sent_data[i].is_ack == false) {
             sent_data[i].is_sent = true;
             data.push_back(sent_data[i]);
             count++;
@@ -34,8 +33,8 @@ vector<Packet> NewReno::SendData(pair<int, bool> lost_packet_verified) {
     return data;
 }
 
-void NewReno::onRTTUpdate(pair<int, bool>& lost_packet_verified) {
-    save_state("tcp_new_reno.csv", curr_time, cwnd);
+void Reno::onRTTUpdate(pair<int, bool>& lost_packet_verified) {
+    save_state("tcp_reno.csv", curr_time, cwnd);
     cout << "cwnd = " << cwnd << "::" << "time = " << curr_time << "::" << "wait time = " << wait_time << endl;
     if (lost_packet_verified.first != -1 && lost_packet_verified.second) {
         curr_time += 0.3 * rtt;
@@ -49,7 +48,7 @@ void NewReno::onRTTUpdate(pair<int, bool>& lost_packet_verified) {
         return;
     }
     else if (lost_packet_verified.first != -1  || wait_time > 0) {
-        if (Reno::timeOut()) {
+        if (TCPConnection::timeOut()) {
             cout << "Time out occured!" << endl;
             wait_time = 0;
             ssthresh = cwnd / 2;
@@ -69,20 +68,13 @@ void NewReno::onRTTUpdate(pair<int, bool>& lost_packet_verified) {
     }
     curr_time += rtt;
 }
-pair<int, bool> NewReno::onPacketLost(std::vector<Packet>& packets, pair<int, bool> last_lost_packet_verified) {
+pair<int, bool> Reno::onPacketLost(std::vector<Packet>& packets, pair<int, bool> last_lost_packet_verified) {
     int lost_count = 0;
     int dup_ack = 0;
-    vector<Packet> sack_packets;
     pair<int, bool> lost_packet_verified = {-1, false};
     if (last_lost_packet_verified.first != -1)
         return last_lost_packet_verified;
-    for (int i = 0; i < packets.size(); i++) {
-        if (packets[i].is_sack && !packets[i].is_ack && lost_packet_verified.first != -1) {
-            window.last_ack++;
-            packets[i].is_ack = true;
-            sent_data[packets[i].index].is_ack = true;
-            continue;
-        }
+    for (int i = 0; i < (int) packets.size(); i++) {
         if (loose_packet(cwnd, 1, 1000)) {
             if (lost_packet_verified.first == -1) {
                 lost_packet_verified.first = packets[i].index;
@@ -92,14 +84,9 @@ pair<int, bool> NewReno::onPacketLost(std::vector<Packet>& packets, pair<int, bo
         }
         else {
             if (lost_packet_verified.first != -1) {
-                packets[i].is_sack = true;
-                sent_data[packets[i].index].is_sack = true;
-                if (fast_retransmit)
-                    sack_packets.push_back(sent_data[packets[i].index]);
                 if (dup_ack == 2 && fast_retransmit) {
                     lost_packet_verified.second = true;
-                    // showSackPackets(sack_packets, lost_packet_verified);
-                    // return lost_packet_verified;
+                    return lost_packet_verified;
                 }
                 else {
                     dup_ack++;
@@ -112,37 +99,41 @@ pair<int, bool> NewReno::onPacketLost(std::vector<Packet>& packets, pair<int, bo
             }
         }
     }
-    showSackPackets(sack_packets, lost_packet_verified);
     return lost_packet_verified;
 }
-
-void NewReno::showSackPackets(const std::vector<Packet>& sack_packet,
-    std::pair<int, bool> last_lost_packet_verified) {
-    if (last_lost_packet_verified.first == -1)
-        return;
-    cout << "Packet " << last_lost_packet_verified.first << "'s ack not received." << endl;
-    cout << "Sack received for packets: ";
-    for (int i = 0; i < sack_packet.size(); i++) {
-        if (i == sack_packet.size())
-            cout << sack_packet[i].index;
+void Reno::showPacketsSent(const std::vector<Packet>& packets) {
+    cout << "Packets sent:" << endl;
+    for (int i = 0; i < (int) packets.size(); i++) {
+        if (i == (int) packets.size() - 1)
+            cout << packets[i].index;
         else
-            cout << sack_packet[i].index << ", ";
+            cout << packets[i].index << ", ";
     }
     cout << endl;
-
 }
-void NewReno::simulate() {
+void Reno::showLostPacket(const pair<int, bool>& lost_packet_verified) {
+    if (lost_packet_verified.first != -1) {
+        cout << "Packet " << lost_packet_verified.first << " lost." << endl;
+        if (lost_packet_verified.second)
+            cout << "3 duplicate Ack received!" << endl;
+    }
+}
+void Reno::showSlidingWindow() {
+    cout << "Sliding window: " << endl;
+    cout << "lastAck = " << window.last_ack << "::" << "last sent = " <<
+        window.last_sent << "::" << "last written = " << window.last_written << endl;
+}
+
+void Reno::simulate() {
     int losts = 0;
     pair<int, bool> lost_packet_verified = { -1, false };
-    Reno::createPackets(100000);
-    while (window.last_ack != window.last_written || sent_data[sent_data.size() - 1].is_sack) {
+    TCPConnection::createPackets(100000);
+    while (window.last_ack != window.last_written) {
         vector<Packet> packets = SendData(lost_packet_verified);
-        Reno::showSlidingWindow();
-        //Reno::showPacketsSent(packets);
+        showSlidingWindow();
         lost_packet_verified = onPacketLost(packets, lost_packet_verified);
-        Reno::showLostPacket(lost_packet_verified);
+        showLostPacket(lost_packet_verified);
         onRTTUpdate(lost_packet_verified);
     }
 }
-
 
--- src/Reno.h
+++ src/Reno.h
@@ -6,15 +6,15 @@
 #include <climits>
 #include <utility>
 #include "Utils.h"
-#include "Reno.h"
+#include "TCPConnection.h"
 #include "SaveState.h"
 
-#ifndef NEW_RENO_H
-#define NEW_RENO_H
+#ifndef RENO_H
+#define RENO_H
 
 //This is Reno
 
-class NewReno :public Reno{
+class Reno : public TCPConnection{
 private:
     // bool fast_retransmit;
     // int cwnd;                    //congestion window
@@ -27,16 +27,15 @@ private:
     // bool fast_recovery;
     // SlidingWindow window;
 public:
-    NewReno();
-    NewReno(int cwnd_, int rtt_, int time_out_, bool fast_retransmit_, bool fast_recovery_);
+    Reno();
+    Reno(int cwnd_, int rtt_, int time_out_, bool fast_retransmit_, bool fast_recovery_);
     virtual std::vector<Packet> SendData(std::pair<int, bool> lost_packet_verified);
     virtual std::pair<int, bool> onPacketLost(std::vector<Packet>& packets, std::pair<int, bool> last_lost_packet_verified);
     virtual void onRTTUpdate(std::pair<int, bool>& lost_packet_verified);
-    // void showPacketsSent(const std::vector<Packet>& packets);
-    // void showLostPacket(const std::pair<int, bool>& lost_packet_verified);
-    // void showSlidingWindow();
-    void showSackPackets(const std::vector<Packet>& sack_packet, std::pair<int, bool> last_lost_packet_verified);
+    void showPacketsSent(const std::vector<Packet>& packets);
+    void showLostPacket(const std::pair<int, bool>& lost_packet_verified);
+    void showSlidingWindow();
     virtual void simulate();
 };
 
-#endif //NEW_RENO_H
+#endif //RENO_H
